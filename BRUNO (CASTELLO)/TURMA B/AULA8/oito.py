6b37400ea0003bb05e06f0a6a62cf5e10194ab48
Bruno Moraes <bruno.conectiva83@icloud.com>
2026-05-05 09:24:18 -0300

    Aula 05.05.26

---
+++ BRUNO (CASTELLO)/TURMA B/AULA8/oito.py
@@ -0,0 +1,11 @@
+# Exercícios de Programação Python: "O Caça-Erros"
+
+# 1. O Problema da Idade
+# Erro
+idade = input("Digite sua idade:")
+if idade >= 18:
+    print("Você é maior de idade")
+
+#Corrigido
+
+#Melhorado
